a4ace4a9ca799f06628c15f8307cbb2a099e136f
Andreas Fischer <git@andreasfischer.net>
2015-10-04 11:15:35 +0200

    Use site-info from hemingway theme instead of hemingway-rewritten.

--- footer.php
+++ footer.php
@@ -12,9 +12,7 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php get_sidebar( 'footer' ); ?>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'hemingway-rewritten' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'hemingway-rewritten' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'hemingway-rewritten' ), 'Hemingway Rewritten', '<a href="http://www.andersnoren.se" rel="designer">Anders Norén</a>' ); ?>
+			&copy; <?php echo date("Y") ?> <a href="<?php echo home_url(); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
